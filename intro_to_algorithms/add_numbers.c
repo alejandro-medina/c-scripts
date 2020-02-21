ae98e54e6fc6afe6a706e9da1d4029bdd90ede95
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2020-02-21 07:57:33 -0600

    Add numbers using function

---
+++ intro_to_algorithms/add_numbers.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Declare functions
+int add_two_nums(int numA, int numB);
+
+int main(int argc, char** argv) {
+	// Convert arguments to numeric value
+	int numA = atoi(argv[1]);
+	int numB = atoi(argv[2]);
+	// call the function
+	int result = add_two_nums(numA, numB);
+
+	printf("The sum of %d + %d is %d\n", numA, numB, result);
+	return 0;
+}
+
+// Define the function
+int add_two_nums(int numA, int numB) {
+
+	return numA + numB;
+
+}
